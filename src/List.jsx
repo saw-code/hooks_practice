6cc19d95cb3b65e39285f69f3fc977ce678a4b73
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-04-06 14:40:21 +0300

    useEffect hook finish practice

--- src/List.jsx
+++ src/List.jsx
@@ -2,18 +2,39 @@ import React, {useEffect, useState} from 'react';
 
 export const List = () => {
   const [numbers, setNumbers] = useState([1, 2, 3])
+  const [count, setCount] = useState(0)
 
   const addNumber = () => {
     const randNumber = Math.round(Math.random() * 10)
     setNumbers([...numbers, randNumber])
   }
-
+  // componentDidUpdate (только для count)
   useEffect(() => {
-    console.log('КОМПОНЕНТ БЫЛ ОТОБРАЖЕН!')
+    console.log('COUNT БЫЛ ОБНОВЛЕН!')
+  }, [count])
+
+  // componentDidUpdate (только для numbers)
+  useEffect(() => {
+    console.log('NUMBERS БЫЛ ОБНОВЛЕН!')
+  }, [numbers])
+
+  // componentDidUpdate (при любом изменении)
+  useEffect(() => {
+    console.log('КОМПОНЕНТ ОБНОВИЛСЯ!')
+  })
+
+  // componentDidMountUpdate + componentWillUnmount
+  useEffect(() => {
+    console.log('КОМПОНЕНТ ОТОБРАЗИЛСЯ!')
+    return () => {
+      console.log('КОМПОНЕНТ БЫЛ УДАЛЕН!')
+    }
   }, [])
 
   return (
     <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
       <ul>
         {numbers.map((el, index) => <li key={index}>{el}</li>)}
       </ul>
@@ -23,7 +44,7 @@ export const List = () => {
 };
 
 
-// реализация классовой компоненты
+// реализация useEffect в классовой компоненты
 // export class List extends React.Component {
 //
 //   state = {
