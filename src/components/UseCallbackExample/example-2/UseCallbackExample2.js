4900ca189fcef4d0a1990f76db9632364e7a20bb
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-04-07 22:25:32 +0300

    fix comments inside useCallback hook

--- src/components/UseCallbackExample/example-2/UseCallbackExample2.js
+++ src/components/UseCallbackExample/example-2/UseCallbackExample2.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useMemo, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import './useCallbackCSS.css';
 import {Buttons} from "./Button";
 
@@ -6,7 +6,13 @@ import {Buttons} from "./Button";
 export const UseCallbackExample2 = () => {
   const [count, setCounter] = useState(0);
   // ссылки не меняются потому что useCallback сохраняет ссылки при первом рендере, но useCallback
-  // не предотвращает лишние ререндеры компоненты Button
+  // не предотвращает лишние ререндеры компоненты Button.
+  // Отличие useMemo от useCallback в том, что useMemo мемоизирует то, что возвращает ф-ия,
+  // а useCallback мемоизирует саму функцию. В нашем случае зависимости мы не указываем по этому
+  // мемоизирует только один раз при первом рендеринге. Если бы мы указали зависимость, например
+  // count, то у нас бы рендерилалсь компонента Buttons потому что функция мемоизировалась
+  // каждый раз когда в ней изменялся бы параметр count, соответственно постоянно обновлялись
+  // бы пропсы
   const onPlus = useCallback(() => setCounter((count) => count + 1), [])
 
   const onMinus = useCallback(() => setCounter((count) => count - 1), [])
